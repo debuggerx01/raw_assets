88d09667657a3ec726bdf0bdaff7dde3297df928
debuggerx01 <dx8917312@163.com>
2023-07-13 10:40:04 +0800

    Update rsm_install.sh

--- scripts/rsm_install.sh
+++ scripts/rsm_install.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 
 
-APP_URL=https://www.debuggerx.com/raw_assets/bin/rsm_server_x64
+APP_URL=https://www.debuggerx.com/raw_assets/bin/rsm_server
 
 HOME_DIR=$HOME
 EXE_DIR=$HOME_DIR/.local/share/remote_system_monitor
 EXE_FILE=$EXE_DIR/server
-AUTOSTART_FILE=$HOME_DIR/.config/autostart/rsm_server.desktop
+AUTOSTART_DIR=$HOME_DIR/.config/autostart
+AUTOSTART_FILE=$AUTOSTART_DIR/rsm_server.desktop
+SERVICE_FILE=/usr/lib/systemd/system/rsm_server.service
 
 function download() {
+  ARCH=$(uname -m)
+  if [[ $ARCH == "x86_64" ]]; then
+    ARCH="x64"
+  elif [[ $ARCH == "aarch64" ]]; then
+    ARCH="arm64"
+  else
+    echo "不支持的CPU架构：[$ARCH]"
+    exit 1
+  fi
   echo "开始下载服务端程序"
-  wget $APP_URL -O "$EXE_FILE"
+  wget "$APP_URL"_"$ARCH" -O "$EXE_FILE"
   sudo chmod a+x $EXE_FILE
   VERSION=$($EXE_FILE -v)
   if [[ $VERSION -gt 0 ]]; then
@@ -32,6 +43,8 @@ else
 fi
 
 echo '-----------------------------------------------------'
+
+mkdir -p $AUTOSTART_DIR
 
 declare -A INSTALL_CMDS;
 INSTALL_CMDS[/etc/redhat-release]="yum install"
@@ -90,7 +103,8 @@ else
   download
 fi
 
-read -rp "请输入服务端监听的端口号[1024~65535]，或者直接回车使用默认端口:" PORT
+echo "请输入服务端监听的端口号[1024~65535]，或者直接回车使用默认端口:"
+read -rp "[1024~65535]" PORT
 
 if [[ $PORT == '' ]]; then
   PORT=9999
@@ -106,8 +120,15 @@ if [[ $PORT -lt 1024 ]]; then
   exit 1
 fi
 
+echo "请选择服务运行模式："
+echo "1. 桌面模式 - 服务将会在登录桌面后以当前用户身份自动启动"
+echo "2. 服务器模式 - 服务将会在系统启动后以root身份自动启动"
+echo "请根据您的实际情况，输入1或2后回车"
+read -rp "[1/2]" MODE
 
-cat > $AUTOSTART_FILE << EOF
+if [[ $MODE -eq 1 ]]; then
+  echo "服务将以桌面模式安装"
+  cat > $AUTOSTART_FILE << EOF
 [Desktop Entry]
 Version=1.0
 Encoding=UTF-8
@@ -118,12 +139,37 @@ Terminal=false
 Type=Application
 Categories=
 GenericName=
+EOF
+
+  chmod a+x $AUTOSTART_FILE
+
+  echo "服务端程序已正确配置！今后将在每次进入桌面时自动运行~"
+elif [[ $MODE -eq 2 ]]; then
+  echo "服务将以服务器模式安装"
+  cat > /tmp/rsm_server.service << EOF
+[Unit]
+Description=server of remote system monitor
+Wants=network-online.target
+After=network.target
+
+[Service]
+ExecStart=$EXE_FILE -p $PORT
+
+[Install]
+WantedBy=multi-user.target
 
 EOF
 
-chmod a+x $AUTOSTART_FILE
+sudo mv /tmp/rsm_server.service $SERVICE_FILE
+sudo systemctl daemon-reload
+sudo systemctl enable rsm_server.service
 
-echo "服务端程序已正确配置！今后将在每次进入桌面时自动运行~"
+  echo "服务端程序已正确配置！今后将在每次开机时自动运行~"
+else
+  echo "模式选在错误，退出安装！！！"
+  exit 1
+fi
+
 
 echo "您可以扫描下方二维码下载安卓端APP:"
 
